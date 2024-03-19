ddcbea076274aeaee36029abd34d9e5e255a0174
Mashrur Hossain Khan <khanmashrur03@gmail.com>
2024-03-19 11:53:12 +0600

    mongodb connection

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 const options = {
   origin: 'http://localhost:3000',
   useSuccessStatus: 200, //because some old servers need it
 };
 const { readdirSync } = require('fs');
 app.use(cors(options)); //every access to this server from anywhere
-/*
-const userRouter = require('./routes/users');
-app.use('/user', userRouter);
-Instead of these two lines, we can user:
-*/
-readdirSync('./routes').map((r) => app.use('/', require('./routes/' + r)));
-/*
-app.get('/', (req, res) => {
-  res.send('home');
-});
 
-app.get('/books', (req, res) => {
-  res.send('books');
-});
-*/
-app.listen(8000, () => {
+//routers
+readdirSync('./routes').map((r) => app.use('/', require('./routes/' + r)));
+
+//database
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('Database connected successfully!'))
+  .catch((err) => console.log('error in mongodb: ' + err));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
   console.log('Server is listening');
 });
